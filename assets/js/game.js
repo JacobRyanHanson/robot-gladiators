176a603152f846d3298508084c2e1e1798d17c80
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-06 19:46:18 -0500

    Create additional enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,25 +1,32 @@
 var playerName = "asdf"; //window.prompt("What is your robot's name?");
-var playerHealth = 12;
+var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roberto";
-var enemyHealth = 50;
+var enemyNames = ["Roborto", "Android", "Trumble"];
+var enemyHealth = 20;
 var enemyAttack = 12;
 
 window.alert("Welcome to Robot Gladiators!");
 
-while (playerHealth > 0) {
-    battle();
-}
+for (var i = 0; i < enemyNames.length; i++) {
+    while (playerHealth > 0 && enemyHealth > 0) {
+        battle(enemyNames[i]);
+    }
 
-function battle() {
+    enemyHealth = 20;
+    enemyAttack = 12;
+}        
+
+function battle(enemyName) {
     var promptFight = window.prompt("Would you like to [Fight] or [Skip] this battle? Enter 'Fight' or 'Skip' to choose.");
 
     if ("fight".localeCompare(promptFight, 'en', {sensitivity: 'accent'}) === 0) {
         enemyHealth -= playerAttack;
 
+        window.alert("A wild " + enemyName + " has appeared.");
         if (enemyHealth > 0) {
+            console.log("--------------------------------")
             console.log(playerName + " attacked " + enemyName);
             console.log(playerName + " delt " + playerAttack + " damage.");
             console.log(enemyName + " has " + enemyHealth + " health remaining.");
@@ -30,6 +37,7 @@ function battle() {
             playerHealth -= enemyAttack;
 
             if (playerHealth > 0) {
+                console.log("--------------------------------")
                 console.log(enemyName + " attacked " + playerName);
                 console.log(enemyName + " delt " + enemyAttack + " damage.");
                 console.log(playerName + " has " + playerHealth + " health remaining.");
