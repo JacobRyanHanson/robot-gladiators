7b26987072f3e99a505d8c65986bed5190d93d0d
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-06 21:14:20 -0500

    Add more rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,18 +4,23 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Android", "Trumble"];
-var enemyHealth = 20;
+var enemyHealth = 50;
 var enemyAttack = 12;
+
+var round = 0;
 
 window.alert("Welcome to Robot Gladiators!");
 
-for (var i = 0; i < enemyNames.length; i++) {
+for (var i = 0; i < enemyNames.length && playerHealth > 0; i++) {
+    window.alert("A wild " + enemyNames[i] + " has appeared.");
+
     while (playerHealth > 0 && enemyHealth > 0) {
+        round++;
+        window.alert("Round " + round);
         battle(enemyNames[i]);
     }
 
-    enemyHealth = 20;
-    enemyAttack = 12;
+    enemyHealth = 50;
 }        
 
 function battle(enemyName) {
@@ -24,29 +29,32 @@ function battle(enemyName) {
     if ("fight".localeCompare(promptFight, 'en', {sensitivity: 'accent'}) === 0) {
         enemyHealth -= playerAttack;
 
-        window.alert("A wild " + enemyName + " has appeared.");
+        console.log("--------------------------------")
+        console.log(playerName + " attacked " + enemyName);
+        console.log(playerName + " delt " + playerAttack + " damage.");
+
+        window.alert(playerName + " attacked.")
+
         if (enemyHealth > 0) {
-            console.log("--------------------------------")
-            console.log(playerName + " attacked " + enemyName);
-            console.log(playerName + " delt " + playerAttack + " damage.");
             console.log(enemyName + " has " + enemyHealth + " health remaining.");
 
-            window.alert(playerName + " attacked.")
             window.alert(enemyName + " has " + enemyHealth + " health left.");
 
             playerHealth -= enemyAttack;
 
+            console.log("--------------------------------")
+            console.log(enemyName + " attacked " + playerName);
+            console.log(enemyName + " delt " + enemyAttack + " damage.");
+
+            window.alert(enemyName + " attacked.");
+
             if (playerHealth > 0) {
-                console.log("--------------------------------")
-                console.log(enemyName + " attacked " + playerName);
-                console.log(enemyName + " delt " + enemyAttack + " damage.");
-                console.log(playerName + " has " + playerHealth + " health remaining.");
+                console.log(playerName + " has " + playerHealth + " health remaining.");              
                 
-                window.alert(enemyName + " attacked.");
                 window.alert(playerName + " has " + playerHealth + " health left.");
             }  
             else {
-                window.alert(playerName + " has died!");
+                window.alert(playerName + " has died in battle! Game Over!");
             }  
         }
         else {
@@ -54,11 +62,12 @@ function battle(enemyName) {
         }     
     }
     else if ("skip".localeCompare(promptFight, 'en', {sensitivity: 'accent'}) === 0) {
-        var confrimSkip = window.confirm("Are you sure you'd like to quit?");
+        var confirmSkip = window.confirm("Are you sure you'd like to skip?");
 
         if (confirmSkip) {
             window.alert(playerName + " has decided to skip this fight.");
-            playerMoney -= 2;
+            playerMoney -= 10;
+            enemyHealth = 0;
         }
     }
     else {
