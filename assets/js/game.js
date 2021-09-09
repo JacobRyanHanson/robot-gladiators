2f47ecdf42c0dc1974f041eeeba2eb25b724a311
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-09 07:53:47 -0500

    Add getName function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,14 @@
 //objects
+debugger;
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getName(),
     health: 100,
     attack: 10,
-    money: 10,
+    money: 30,
     reset: function() {
         this.health = 100;
         this.attack = 10;
-        this.money = 10;
+        this.money = 30;
     },
     refillHealth: function() {
         this.health += 20;
@@ -34,29 +35,37 @@ var enemyInfo = [
     }
 ]   
 
-window.alert("Welcome to Robot Gladiators!");
+alert("Welcome to Robot Gladiators!");
 
 do {
     playerInfo.reset();
     for (var i = 0; i < enemyInfo.length && playerInfo.health > 0; i++) {
         enemyInfo[i].health = randNum(40, 60);
-        window.alert("Round " + (i + 1));
-        window.alert("A wild " + enemyInfo[i].name + " has appeared.");
+        alert("Round " + (i + 1));
+        alert("A wild " + enemyInfo[i].name + " has appeared.");
         battle(enemyInfo[i]);
         if (i < enemyInfo.length - 1 && playerInfo.health > 0) {
-            var shopConfirm = window.confirm("The shop is open. Would you like to visit?");
+            var shopConfirm = confirm("The shop is open. Would you like to visit?");
             if (shopConfirm) {
                 shop();
             }   
         }    
     }
     endGame();
-    var playAgain = window.confirm("Would you like to play again?");
+    var playAgain = confirm("Would you like to play again?");
 } while (playAgain === true);
+
+function getName() {
+    var name;
+    do {
+        name = prompt("What is your robot's name?");
+    } while (name === "" || name === null);
+    return name;
+}
 
 function battle(enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
-        var promptFight = window.prompt("Would you like to [Fight] or [Skip] this battle? Enter 'Fight' or 'Skip' to choose.");
+        var promptFight = prompt("Would you like to [Fight] or [Skip] this battle? Enter 'Fight' or 'Skip' to choose.");
 
         if ("fight".localeCompare(promptFight, 'en', {sensitivity: 'accent'}) === 0) {
             var damage = randNum(playerInfo.attack - 3, playerInfo.attack);
@@ -65,11 +74,11 @@ function battle(enemy) {
             console.log("--------------------------------");
             console.log(playerInfo.name + " attacked " + enemy.name);
             console.log(playerInfo.name + " delt " + damage + " damage.");
-            window.alert(playerInfo.name + " attacked.");
+            alert(playerInfo.name + " attacked.");
 
             console.log(enemy.name + " has " + enemy.health + " health remaining.");
             if (enemy.health > 0) {
-                window.alert(enemy.name + " has " + enemy.health + " health left.");
+                alert(enemy.name + " has " + enemy.health + " health left.");
 
                 var damage = randNum(enemy.attack - 3, enemy.attack);
                 playerInfo.health = Math.max(0, playerInfo.health - damage);
@@ -77,56 +86,56 @@ function battle(enemy) {
                 console.log("--------------------------------");
                 console.log(enemy.name + " attacked " + playerInfo.name);
                 console.log(enemy.name + " delt " + damage + " damage.");
-                window.alert(enemy.name + " attacked.");
+                alert(enemy.name + " attacked.");
 
                 console.log(playerInfo.name + " has " + playerInfo.health + " health remaining."); 
                 if (playerInfo.health > 0) {               
-                    window.alert(playerInfo.name + " has " + playerInfo.health + " health left.");
+                    alert(playerInfo.name + " has " + playerInfo.health + " health left.");
                 }
                 else {
-                    window.alert(playerInfo.name + " has died.");
+                    alert(playerInfo.name + " has died.");
                 }
             }
             else {
-                window.alert(enemy.name + " has died.");
+                alert(enemy.name + " has died.");
             }
         }
         else if ("skip".localeCompare(promptFight, 'en', {sensitivity: 'accent'}) === 0) {
-            var confirmSkip = window.confirm("Are you sure you'd like to skip?");
+            var confirmSkip = confirm("Are you sure you'd like to skip?");
 
             if (confirmSkip && playerInfo.money >= 10) {
-                window.alert(playerInfo.name + " has decided to skip this fight.");
+                alert(playerInfo.name + " has decided to skip this fight.");
                 playerInfo.money = Math.max(0, playerInfo.money - 10);
                 break;
             }
             else {
-                window.alert(playerInfo.name + " has insufficient currency to skip.");
+                alert(playerInfo.name + " has insufficient currency to skip.");
             }
         }
         else {
-            window.alert("Invalid input. Please enter 'Fight' or 'Skip'.");
+            alert("Invalid input. Please enter 'Fight' or 'Skip'.");
         }
     }  
 };
 
 function shop() {
-    var shopChoice = window.prompt("Would you like to [Refill] your health, [Upgrade] your attack or [Leave] the store?");
+    var shopChoice = prompt("Would you like to [Refill] your health, [Upgrade] your attack or [Leave] the store?");
     if ("refill".localeCompare(shopChoice, 'en', {sensitivity: 'accent'}) === 0 && playerInfo.money >= 7) {
-        window.alert("Refilling " + playerInfo.name + "'s health by 20 for 7 currency.")
+        alert("Refilling " + playerInfo.name + "'s health by 20 for 7 currency.")
         playerInfo.refillHealth();
     }
     else if ("upgrade".localeCompare(shopChoice, 'en', {sensitivity: 'accent'}) === 0 && playerInfo.money >= 7) {
-        window.alert("Upgrading " + playerInfo.name + "'s attack by 6 points for 7 currency.")
+        alert("Upgrading " + playerInfo.name + "'s attack by 6 points for 7 currency.")
         playerInfo.upgradeAttack();
     }
     else if ("leave".localeCompare(shopChoice, 'en', {sensitivity: 'accent'}) === 0) {
-        window.alert(playerInfo.name + " left the store.");
+        alert(playerInfo.name + " left the store.");
     }
     else if (playerInfo.money < 7) {
-        window.alert(playerInfo.name + " has insufficient currency to purchase.");
+        alert(playerInfo.name + " has insufficient currency to purchase.");
     }
     else {
-        window.alert("Invalid input. Please enter 'Refill', 'Upgrade', or 'Leave'.")
+        alert("Invalid input. Please enter 'Refill', 'Upgrade', or 'Leave'.")
         shop();
     }
 };
@@ -138,9 +147,9 @@ function randNum(min, max) {
 
 function endGame() {
     if (playerInfo.health > 0) {
-        window.alert("Final Score: " + playerInfo.money)
+        alert("Final Score: " + playerInfo.money)
     }
     else {
-        window.alert("Game Over");
+        alert("Game Over");
     }
 };
