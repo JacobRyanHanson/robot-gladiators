13e88e3279352a3e317c82657d4e5463f83b1774
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-09 20:38:59 -0500

    Remove skip from global

--- assets/js/game.js
+++ assets/js/game.js
@@ -34,10 +34,9 @@ var enemyInfo = [
         name: "Trumble",
         attack: randNum(10, 14)
     }
-] 
+]
 
-var skip;
-
+//main game loop
 do {
     playerInfo.reset();
     for (var i = 0; i < enemyInfo.length && playerInfo.health > 0; i++) {
@@ -56,24 +55,25 @@ do {
     var playAgain = confirm("Would you like to play again?");
 } while (playAgain === true);
 
+//runs one round of battle between player and bot
 function battle(enemy) {
     var number = randNum(0, 1);
     var otherIndex = otherIndexCheck(number);
     var turnOrder = [number, otherIndex]
-    skip = false; //skip reset
+    var skip = false; //skip reset
 
     while (playerInfo.health > 0 && enemy.health > 0 && !skip) {
         for (var i = 0; i < turnOrder.length  && !skip; i++) {
             if (turnOrder[i] === 0) {
-                playerTurn(enemy);
-            }
-            else {
+                skip = playerTurn(enemy);
+            } else {
                 enemyTurn(enemy);
             }
         }
     }  
 }
 
+//gets a valid username
 function getName() {
     var name;
     do {
@@ -82,15 +82,16 @@ function getName() {
     return name;
 }
 
+//assigns index 1 of turnOrder based on the first random number
 function otherIndexCheck(number) {
     if (number === 1) {
         return 0;
-    }
-    else {
+    } else {
         return 1;
     }
 }
 
+//runs the players turn allowing them to fight or skip
 function playerTurn(enemy) {
     var promptFight = prompt("Would you like to [Fight] or [Skip] this battle? Enter 'Fight' or 'Skip' to choose.");
 
@@ -107,30 +108,27 @@ function playerTurn(enemy) {
 
         if (enemy.health > 0) {               
             alert(enemy.name + " has " + enemy.health + " health left.");
-        }
-        else {
+        } else {
             alert(enemy.name + " has died.");
         }
-    }
-    else if ("skip".localeCompare(promptFight, 'en', {sensitivity: 'accent'}) === 0) {
+    } else if ("skip".localeCompare(promptFight, 'en', {sensitivity: 'accent'}) === 0) {
         var confirmSkip = confirm("Are you sure you'd like to skip?");
 
         if (confirmSkip && playerInfo.coins >= 10) {
             alert(playerInfo.name + " has decided to skip this fight.");
             playerInfo.coins = Math.max(0, playerInfo.coins - 10);
-            skip = true;
-        }
-        else {
+            return true; //skip boolean
+        } else {
             alert(playerInfo.name + " has insufficient coins to skip.");
             playerTurn(enemy);
         }
-    }
-    else {
+    } else {
         alert("Invalid input. Please enter 'Fight' or 'Skip'.");
         playerTurn(enemy);
     }
 }
 
+//runs the enemys turn allowing it to attack
 function enemyTurn(enemy) {
     if (enemy.health > 0) {
         var damage = randNum(enemy.attack - 3, enemy.attack);
@@ -145,33 +143,28 @@ function enemyTurn(enemy) {
         
         if (playerInfo.health > 0) {               
             alert(playerInfo.name + " has " + playerInfo.health + " health left.");
-        }
-        else {
+        } else {
             alert(playerInfo.name + " has died.");
         }
-    }
-    else {
+    } else {
         alert(enemy.name + " has died.");
     }
 }
 
+//runs the shop allowing the player to refill health, upgrade attack, or leave
 function shop() {
     var shopChoice = prompt("Would you like to [1] refill your health, [2] upgrade your attack or [3] leave the store?");
     if (parseInt(shopChoice) === 1 && playerInfo.coins >= 7) {
         alert("Refilling " + playerInfo.name + "'s health by 20 for 7 coins.");
         playerInfo.refillHealth();
-    }
-    else if (parseInt(shopChoice) === 2 && playerInfo.coins >= 7) {
+    } else if (parseInt(shopChoice) === 2 && playerInfo.coins >= 7) {
         alert("Upgrading " + playerInfo.name + "'s attack by 6 points for 7 coins.");
         playerInfo.upgradeAttack();
-    }
-    else if (parseInt(shopChoice) === 3) {
+    } else if (parseInt(shopChoice) === 3) {
         alert(playerInfo.name + " left the store.");
-    }
-    else if (playerInfo.coins < 7) {
+    } else if (playerInfo.coins < 7) {
         alert(playerInfo.name + " has insufficient coins to purchase anything.");
-    }
-    else {
+    } else {
         alert("Invalid input. Please enter '1', '2', or '3'.");
         shop();
     }
@@ -185,8 +178,7 @@ function randNum(min, max) {
 function endGame() {
     if (playerInfo.health > 0) {
         alert("Final Score: " + playerInfo.coins);
-    }
-    else {
+    } else {
         alert("Game Over");
     }
 }
